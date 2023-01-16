03c5944233a4dd824e90f6e6c863568c31a43bb1
Byiringiro Saad <byiringirosaad@gmail.com>
2023-01-16 12:05:04 +0200

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,24 @@
+import styled from "styled-components";
 import React, { useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+
+//icons
+import { RiHome6Fill } from "react-icons/ri";
+import { HiUserGroup } from "react-icons/hi";
+import {
+  BsFillChatFill,
+  BsBellFill,
+  BsFillCaretDownFill,
+} from "react-icons/bs";
 
 const Nav = () => {
+  //configs
   const location = useLocation();
   const navigate = useNavigate();
+
+  //local data
   const [active, setActive] = React.useState("");
+  const [authenticated, setAuthenticated] = React.useState(true);
 
   useEffect(() => {
     setActive(location.pathname);
@@ -15,29 +28,74 @@ const Nav = () => {
     navigate("/");
   };
 
+  const goTo = (path) => {
+    navigate(path);
+  };
+
   return (
     <Container>
-      <div className="content">
-        <div className="logo" onClick={goHome}>
-          <img src="/min_logo.svg" alt="logo" />
+      {authenticated ? (
+        <div className="content">
+          <div className="logo" onClick={goHome}>
+            <img src="/min_logo.svg" alt="logo" />
+          </div>
+          <div className="nav">
+            <Link to="/home" className={active === "/home" ? "active" : ""}>
+              <RiHome6Fill className="one icon" />
+            </Link>
+            <Link
+              to="/messages"
+              className={active === "/messages" ? "active" : ""}
+            >
+              <BsFillChatFill className="two icon" />
+            </Link>
+            <Link
+              to="/members"
+              className={active === "/members" ? "active" : ""}
+            >
+              <HiUserGroup className="three icon" />
+            </Link>
+          </div>
+          <div className="buttons">
+            <div
+              className="notification"
+              onClick={() => goTo("/notifications")}
+            >
+              <BsBellFill className="icon" />
+            </div>
+            <div className="profile">
+              <div className="image"></div>
+              <p className="name">Byiringiro</p>
+              <BsFillCaretDownFill className="icon" />
+            </div>
+          </div>
         </div>
-        <div className="nav">
-          <Link to="/" className={active === "/" ? "active" : ""}>
-            Home
-          </Link>
-          <Link to="/contact" className={active === "/contact" ? "active" : ""}>
-            Contact
-          </Link>
+      ) : (
+        <div className="content">
+          <div className="logo" onClick={goHome}>
+            <img src="/min_logo.svg" alt="logo" />
+          </div>
+          <div className="nav">
+            <Link to="/" className={active === "/" ? "active" : ""}>
+              Home
+            </Link>
+            <Link
+              to="/contact"
+              className={active === "/contact" ? "active" : ""}
+            >
+              Contact
+            </Link>
+          </div>
+          <div className="buttons">
+            <Link to="/login" className={active === "/login" ? "active" : ""}>
+              Login
+            </Link>
+            <Link to="/signup" className="signup">
+              Signup
+            </Link>
+          </div>
         </div>
-        <div className="buttons">
-          <Link to="/login" className={active === "/login" ? "active" : ""}>
-            Login
-          </Link>
-          <Link to="/signup" className="signup">
-            Signup
-          </Link>
-        </div>
-      </div>
+      )}
     </Container>
   );
 };
@@ -102,6 +160,22 @@ const Container = styled.div`
       margin: 0 30px;
       text-decoration: none;
       color: var(--dark);
+
+      .icon {
+        color: var(--dark);
+      }
+
+      .one {
+        font-size: 2em;
+      }
+
+      .two {
+        font-size: 1.7em;
+      }
+
+      .three {
+        font-size: 2em;
+      }
     }
   }
 
@@ -112,6 +186,47 @@ const Container = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: flex-end;
+
+    .notification {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin: 0 10px 0 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: var(--gray);
+      cursor: pointer;
+
+      .icon {
+        font-size: 1.4em;
+      }
+    }
+
+    .profile {
+      width: 150px;
+      height: 40px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 0 7px;
+      border-radius: 50px;
+      background: var(--gray);
+      cursor: pointer;
+
+      .image {
+        width: 30px;
+        height: 30px;
+        margin: 0 7px 0 0;
+        border-radius: 50%;
+        background: var(--dark);
+      }
+
+      .icon {
+        margin: 0 0 0 15px;
+      }
+    }
 
     a {
       margin: 0 0 0 30px;
