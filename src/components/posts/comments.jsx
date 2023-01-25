415c90aba257bfbc3847f82bb81cbba46842c637
Byiringiro Saad <byiringirosaad@gmail.com>
2023-01-25 21:34:35 +0200

    Update comments.jsx

--- src/components/posts/comments.jsx
+++ src/components/posts/comments.jsx
@@ -2,9 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 //icons
+import { BsEmojiHeartEyesFill } from "react-icons/bs";
 
 const Comments = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <div className="search">
+        <div className="user">
+          <img src="/images/user.jpg" alt="user" />
+        </div>
+        <div className="input">
+          <input type="text" placeholder="Type something...." />
+          <BsEmojiHeartEyesFill className="icon" />
+        </div>
+      </div>
+      <div className="comments">
+        <div className="all">
+          <div className="comment">
+            <div className="user">
+              <img src="/images/user.jpg" alt="user" />
+            </div>
+            <div className="comment_content"></div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -13,6 +36,54 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  .search {
+    width: 100%;
+    height: 50px;
+    margin: 20px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
+
+    .user {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      overflow: hidden;
+    }
+
+    .input {
+      width: calc(100% - 60px);
+      height: 50px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1px solid var(--gray);
+
+      input {
+        width: 80%;
+        height: 100%;
+        border: none;
+        outline: none;
+        background: transparent;
+        padding: 0 0 0 10px;
+      }
+
+      .icon {
+        width: 35px;
+        height: 35px;
+        padding: 7px;
+        border-radius: 50%;
+        cursor: pointer;
+
+        :hover {
+          background: var(--gray);
+        }
+      }
+    }
+  }
 `;
 
 export default Comments;
