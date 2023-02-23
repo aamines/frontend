7d019404a659999d9c05195ab2387613b97af352
Dukunde Irakoze Happy <acele4444@gmail.com>
2023-02-23 23:52:09 +0200

    comments done

--- src/components/posts/comments.jsx
+++ src/components/posts/comments.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import './comments.css'
 
 //icons
 import { BsEmojiHeartEyesFill } from "react-icons/bs";
+import { BiUpvote } from "react-icons/bi";
+import { MdModeComment } from "react-icons/md";
+import { BsHandThumbsUpFill } from "react-icons/bs";
 
 const Comments = () => {
   return (
@@ -16,15 +20,76 @@ const Comments = () => {
           <BsEmojiHeartEyesFill className="icon" />
         </div>
       </div>
+
+      {/* comments */}
+
       <div className="comments">
-        <div className="all">
-          <div className="comment">
-            <div className="user">
-              <img src="/images/user.jpg" alt="user" />
-            </div>
-            <div className="comment_content"></div>
-          </div>
+        <img src="/images/user.jpg" alt="user" className="commenterImg"/>
+        <div className="commentContent">Happy for the team behind the product, we have been really waiting 
+        for this and we can all agree that it doesn’t dissapoint, Good luck along the way😊😊!</div>
+      </div>
+      <div className="commentReactions">
+      <div className="reactions">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <p className="f-low">356</p>
         </div>
+        <div className="commentstats">
+          <MdModeComment className="icon" />
+          <p className="f-low">100</p>
+        </div>
+
+        <p className="f-left">Load replies</p>
+      </div>
+
+      <div className="comments">
+        <img src="/images/user.jpg" alt="user" className="commenterImg"/>
+        <div className="commentContent">Happy for the team behind the product, we have been really waiting 
+        for this and we can all agree that it doesn’t dissapoint, Good luck along the way😊😊!</div>
+      </div>
+      <div className="commentReactions">
+      <div className="reactions">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <p className="f-low">356</p>
+        </div>
+        <div className="commentstats">
+          <MdModeComment className="icon" />
+          <p className="f-low">100</p>
+        </div>
+
+        <p className="f-left">Load replies</p>
+        
+      </div>
+      <div className="comments">
+        <img src="/images/user.jpg" alt="user" className="commenterImg"/>
+        <div className="commentContent">Happy for the team behind the product, we have been really waiting 
+        for this and we can all agree that it doesn’t dissapoint, Good luck along the way😊😊!</div>
+      </div>
+      <div className="commentReactions">
+      <div className="reactions">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <p className="f-low">356</p>
+        </div>
+        <div className="commentstats">
+          <MdModeComment className="icon" />
+          <p className="f-low">100</p>
+        </div>
+
+        <p className="f-left">Load replies</p>
+
+        <br/>
+        
       </div>
     </Container>
   );
