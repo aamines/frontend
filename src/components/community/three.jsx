995ffece7b4799cbdeefafdd2e6a8b475e0ffa14
Byiringiro Saad <byiringirosaad@gmail.com>
2023-04-18 09:53:29 +0200

    Update three.jsx

--- src/components/community/three.jsx
+++ src/components/community/three.jsx
@@ -1,8 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const Three = () => {
+  //local data
+  const [loading, setLoading] = useState(false);
+  const data = useSelector((state) => state.community.create);
+
+  const handleSubmit = () => {
+    setLoading(true);
+  };
+
   return (
     <Container>
       <p>
@@ -10,7 +19,11 @@ const Three = () => {
         experience.
       </p>
       <div className="button">
-        <Link to="/home">Enjoy</Link>
+        {loading ? (
+          <img src="/loader.svg" alt="loader" />
+        ) : (
+          <Link to="/home">Enjoy</Link>
+        )}
       </div>
     </Container>
   );
@@ -31,24 +44,27 @@ const Container = styled.div`
   }
 
   .button {
-    width: auto;
+    width: 150px;
     height: 40px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     margin: 20px 0;
+    border-radius: 5px;
+    background: var(--bright);
 
     a {
-      width: 150px;
       height: 100%;
       display: flex;
       align-items: center;
       justify-content: center;
       text-decoration: none;
       color: var(--dark);
-      border-radius: 5px;
-      background: var(--bright);
+    }
+
+    img {
+      width: 35%;
     }
   }
 `;
