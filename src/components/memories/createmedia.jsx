87604fe2ed918d00fb0cae96a0ee6c2433c3b0f5
Byiringiro Saad <byiringirosaad@gmail.com>
2023-04-20 09:09:26 +0200

    create memory media

--- src/components/memories/createmedia.jsx
+++ src/components/memories/createmedia.jsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { useNavigate } from "react-router";
 import { useDropzone } from "react-dropzone";
 
 //icons
-import uploadImg from "../../assets/upload.png";
+import { FiUpload } from "react-icons/fi";
 
 const CreateMedia = () => {
-  const navigate = useNavigate();
-  const goTo = (path) => {
-    navigate(path);
-  };
-
   //drag and drop image
   const [image, setImage] = useState([]);
   const { getRootProps, getInputProps } = useDropzone({
@@ -29,125 +23,113 @@ const CreateMedia = () => {
 
   return (
     <Container>
-      <div className="textmedia">
-        <p onClick={() => goTo("/stories/create-text")}>Text</p>
-        <p className="active" onClick={() => goTo("/stories/create-media")}>
-          Media
-        </p>
-      </div>
-      <div className="drop-file-input">
-        <div className="drop-file-input__label" {...getRootProps()}>
+      <div className="container">
+        <div className="upload" {...getRootProps()}>
           <input {...getInputProps()} />
-          {image.length === 0 ? (
-            <img className="upload-icon" src={uploadImg} alt="upload" />
-          ) : (
-            ""
-          )}
+          {image.length === 0 ? <FiUpload className="icon" /> : ""}
           {image.map((upfile) => (
-            <img className="img-preview" src={upfile.preview} alt="preview" />
+            <img src={upfile.preview} alt="preview" />
           ))}
         </div>
       </div>
-      <input className="text" type="text" placeholder="Say Something..." />
-      <button>Send</button>
+      <div className="text">
+        <input type="text" placeholder="Type Something..." />
+        <div className="button">
+          <p>Send</p>
+        </div>
+      </div>
     </Container>
   );
 };
+
 export default CreateMedia;
 
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
 
-  .drop-file-input {
+  .container {
     width: 100%;
+    height: 70%;
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
-  }
-  .drop-file-input__label {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    height: 200px;
-    background: #eaeaea;
-    width: 60%;
-    border-radius: 5px;
-    position: relative;
-    top: 80px;
-    cursor: pointer;
 
-    .upload-icon {
-      width: 50px;
-      height: 50px;
+    .upload {
+      width: 60%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      background: var(--background);
+      border-radius: 5px;
+      cursor: pointer;
+      margin: 50px 0 0 0;
+
+      input {
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        background: purple;
+        bottom: 100px;
+      }
+
+      .icon {
+        font-size: 4em;
+        color: var(--gray);
+      }
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: cover;
+      }
+
+      :hover {
+        opacity: 0.6;
+      }
     }
-    .display {
-      display: none;
-    }
-    .img-preview {
-      position: relative;
-      max-width: 100%;
-      max-height: 100%;
-      object-fit: cover;
-    }
-  }
-  .drop-file-input input {
-    opacity: 0;
-    position: relative;
-    top: -150px;
-    width: 50%;
-    height: 5vh;
-    cursor: pointer;
-    background: purple;
-    bottom: 100px;
-  }
-  .drop-file-input__label:hover,
-  .drop-file-input.dragover {
-    opacity: 0.6;
   }
 
   .text {
     width: 60%;
-    outline: none;
-    border: none;
-    border-bottom: 2px solid #717171;
-    background: none;
-    margin-top: 100px;
-  }
-  .textmedia {
+    height: 100px;
+    margin: 20px 0 0 0;
     display: flex;
-    position: relative;
-    top: 40px;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+    flex-direction: column;
+    align-items: flex-end;
 
-    p {
-      width: 200px;
-      text-align: center;
+    input {
+      width: 100%;
+      height: 50px;
+      border: none;
+      outline: none;
+      padding: 0 10px;
+      color: var(--white);
+      background: transparent;
+      border-bottom: 1px solid var(--gray);
+    }
+
+    .button {
+      width: 140px;
+      height: 50px;
+      display: flex;
+      border-radius: 5px;
+      align-items: center;
+      justify-content: center;
+      margin: 15px 0 0 0;
       cursor: pointer;
-      position: relative;
-      border-bottom: 2px solid #ccc;
-    }
-    .active {
-      border-bottom: 2px solid #3be28f;
-    }
-  }
+      background: var(--grayish);
 
-  button {
-    margin-top: 50px;
-    width: 130px;
-    border-radius: 3px;
-    outline: none;
-    border: none;
-    background: #d9d9d9;
-    padding: 5px;
-    position: relative;
-    float: right;
-    bottom: 32px;
-    left: 270px;
+      p {
+        color: var(--white);
+      }
+    }
   }
 `;
