83d7d404762055ef1579d9e07ac59a2e1d349230
Byiringiro Saad <byiringirosaad@gmail.com>
2023-10-19 00:13:33 +0200

    started

--- app/[lang]/(auth)/login/page.tsx
+++ app/[lang]/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
 export default function LoginPage() {
   return (
-    <div>
-      <p>Login</p>
+    <div className="flex w-full h-[100vh]">
+      <div className="flex-col h-[100%] w-1/2 items-center justify-center">
+        <p>Saad</p>
+      </div>
+      <div className="flex-col h-[100%] w-1/2 bg-white"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
